93fa9e839acd8f86837a8e686ae8b4f98c5e1abb
Tim Maes <tim.maes91@gmail.com>
2023-12-18 15:12:05 +0100

    Add animation for the copter

--- src/ConsoleCopter/Game.cs
+++ src/ConsoleCopter/Game.cs
@@ -72,6 +72,7 @@ internal class Game
 
     private void UpdateGame()
     {
+        copter.UpdateAnimationFrame();
         copter.Update();
         foreach (var pipe in pipes)
         {
--- src/ConsoleCopter/Models/Copter.cs
+++ src/ConsoleCopter/Models/Copter.cs
@@ -5,6 +5,9 @@ public class Copter
     public int PositionX { get; private set; }
     public int PositionY { get; private set; }
 
+    private char[] animationFrames = new char[] { 'x', '+' };
+    private int currentAnimationFrameIndex = 0;
+
     private int velocityY;
     private const int Gravity = 2;
 
@@ -23,6 +26,12 @@ public class Copter
         PositionY = Math.Max(0, Math.Min(Console.WindowHeight - 1, PositionY));
     }
 
+    public void UpdateAnimationFrame()
+    {
+        currentAnimationFrameIndex = (currentAnimationFrameIndex + 1) % animationFrames.Length;
+    }
+
+
     public void Jump()
     {
         velocityY = -5;
@@ -30,10 +39,12 @@ public class Copter
 
     public void Draw(char[,] buffer)
     {
+        char currentFrame = animationFrames[currentAnimationFrameIndex];
+
         if (PositionX >= 0 && PositionX < Console.WindowWidth &&
             PositionY >= 0 && PositionY < Console.WindowHeight)
         {
-            buffer[PositionX, PositionY] = 'H'; // 'H' represents the helicopter
+            buffer[PositionX, PositionY] = currentFrame;
         }
     }
 
